96c444b8694677665f4d6ee7161af8a0d26fb5b8
Jagankumarg <jagan0523@gmail.com>
2024-04-10 18:00:47 +0530

    updated CORS

--- src/main/java/com/mahankali/temple/controller/MahankaliController.java
+++ src/main/java/com/mahankali/temple/controller/MahankaliController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "https://nuvvulabanda-mahanakali-temple-830bea7efb82.herokuapp.com/")
 public class MahankaliController {
 
     @Autowired
