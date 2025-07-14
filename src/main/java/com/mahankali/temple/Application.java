239d4fc764b48014a18458b7d4445447233d062d
Jagankumarg <jagan0523@gmail.com>
2025-07-14 10:23:31 +0530

    test updates test3 main

--- src/main/java/com/mahankali/temple/Application.java
+++ src/main/java/com/mahankali/temple/Application.java
@@ -32,7 +32,7 @@ public class Application {
     @Autowired //test1 test2 test3 test4 test5 test6
     //test7 test8
     //test9 test10 final test
-    //updates test
+    //updates test test test
     private AmountReceivedRepo amountReceivedRepo;
 
     @Autowired
